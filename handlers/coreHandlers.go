7d58dfbe87ce86e0cd7e7290394220a726564795
yigidigulaglati <yigidigulaglati@gmail.com>
2025-10-07 19:22:45 +0300

    handler

--- handlers/coreHandlers.go
+++ handlers/coreHandlers.go
@@ -12,73 +12,71 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-
-type RotateBody struct{
+type RotateBody struct {
 	Angle *int `json:"angle"`
 }
 
-func Rotate(c *fiber.Ctx) error{
+func Rotate(c *fiber.Ctx) error {
 
-	body := c.FormValue(`metadata`);
-	if body == ``{
-		return c.Status(400).JSON(fiber.Map{`message`:`Angle must be set.`});
+	body := c.FormValue(`metadata`)
+	if body == `` {
+		return c.Status(400).JSON(fiber.Map{`message`: `Angle must be set.`})
 	}
-	bodyData := RotateBody{};
-	err := json.Unmarshal([]byte(body), &bodyData);
+	bodyData := RotateBody{}
+	err := json.Unmarshal([]byte(body), &bodyData)
 	if err != nil {
 		slog.Error(`Could not unmarshal rotate body. Error: ` + err.Error())
-		return fiber.NewError(500, `Something went wrong.`); 
+		return fiber.NewError(500, `Something went wrong.`)
 	}
 
-	if bodyData.Angle == nil{
-		slog.Info(`Angle is nil in rotate handler.`);
-		return c.Status(400).JSON(fiber.Map{`message`:`Angle must be set.`})
+	if bodyData.Angle == nil {
+		slog.Info(`Angle is nil in rotate handler.`)
+		return c.Status(400).JSON(fiber.Map{`message`: `Angle must be set.`})
 	}
 
-	file, err := c.FormFile(`image`);
+	file, err := c.FormFile(`image`)
 	if err != nil {
-		return fiber.NewError(500, `Could not open image.`);	 
-	}
-	
-	fileExt := strings.ToLower(filepath.Ext(file.Filename));
-	if fileExt != `png` && fileExt != `jpeg` && fileExt != `jpg`{
-		return fiber.NewError(400, `Expected png or jpg image.`); 
+		return fiber.NewError(500, `Could not open image.`)
 	}
 
-	fileStream ,err := file.Open();
-	if err != nil {
-		return fiber.NewError(500, `Could not open image.`); 
+	fileExt := strings.ToLower(filepath.Ext(file.Filename))
+	if fileExt != `png` && fileExt != `jpeg` && fileExt != `jpg` {
+		return fiber.NewError(400, `Expected png or jpg image.`)
 	}
-	defer fileStream.Close();
-	
-	var decodedImage image.Image;
-	decodedImage, err = imaging.Decode(fileStream);
+
+	fileStream, err := file.Open()
+	if err != nil {
+		return fiber.NewError(500, `Could not open image.`)
+	}
+	defer fileStream.Close()
+
+	var decodedImage image.Image
+	decodedImage, err = imaging.Decode(fileStream)
 
 	if err != nil {
 		slog.Error(`Could noe decode filestream in rotate handler. Error: ` + err.Error())
-		return fiber.NewError(500, `Something went wrong.`); 
+		return fiber.NewError(500, `Something went wrong.`)
 	}
 
 	rotatedImage := imaging.Rotate(decodedImage, float64(*bodyData.Angle), color.White)
-	
-	writer := c.Response().BodyWriter();
-	if fileExt == `png`{
-		err = imaging.Encode(writer, rotatedImage, imaging.PNG);
-	}else{
-		err = imaging.Encode(writer, rotatedImage, imaging.JPEG, imaging.JPEGQuality(85));
+
+	writer := c.Response().BodyWriter()
+	if fileExt == `png` {
+		err = imaging.Encode(writer, rotatedImage, imaging.PNG)
+	} else {
+		err = imaging.Encode(writer, rotatedImage, imaging.JPEG, imaging.JPEGQuality(85))
 	}
 
 	if err != nil {
 		slog.Error(`Could not encode in rotate handler. Error: ` + err.Error())
-		return fiber.NewError(500, `Something went wrong.`); 
+		return fiber.NewError(500, `Something went wrong.`)
 
 	}
 
-	return nil;
+	return nil
 }
 
-
-type CropMetaData struct{
+type CropMetaData struct {
 	MinX *int `json:"minX"`
 	MinY *int `json:"minY"`
 
@@ -86,28 +84,28 @@ type CropMetaData struct{
 	MaxY *int `json:"maxY"`
 }
 
-func Crop(c *fiber.Ctx) error{
-	metadata := c.FormValue(`metadata`);
-	if metadata == ``{
-		return c.Status(400).JSON(fiber.Map{`message`:`Must set bounds to crop image.`})
+func Crop(c *fiber.Ctx) error {
+	metadata := c.FormValue(`metadata`)
+	if metadata == `` {
+		return c.Status(400).JSON(fiber.Map{`message`: `Must set bounds to crop image.`})
 	}
-	data := CropMetaData{};
-	err := json.Unmarshal([]byte(metadata), &data);
+	data := CropMetaData{}
+	err := json.Unmarshal([]byte(metadata), &data)
 	if err != nil {
 		slog.Error(`Could not unmarshal metadata in crop handler. Error: ` + err.Error())
-		return fiber.NewError(500, `Something went wrong.`); 
-	}
-	
-	if data.MaxX == nil || data.MinX == nil || data.MinY == nil || data.MaxY == nil{
-		return c.Status(400).JSON(fiber.Map{`message`:`Must set bounds to crop the image.`})
+		return fiber.NewError(500, `Something went wrong.`)
 	}
 
-	if *data.MaxX < *data.MinX || *data.MaxY < *data.MinY{
-		return c.Status(400).JSON(fiber.Map{`message`:`Invalid bounds.`})
+	if data.MaxX == nil || data.MinX == nil || data.MinY == nil || data.MaxY == nil {
+		return c.Status(400).JSON(fiber.Map{`message`: `Must set bounds to crop the image.`})
 	}
 
-	if *data.MaxX <0  || *data.MinX < 0 || *data.MaxY < 0 || *data.MinY <0 {
-		return c.Status(400).JSON(fiber.Map{`message`:`Invalid bounds.`})
+	if *data.MaxX < *data.MinX || *data.MaxY < *data.MinY {
+		return c.Status(400).JSON(fiber.Map{`message`: `Invalid bounds.`})
+	}
+
+	if *data.MaxX < 0 || *data.MinX < 0 || *data.MaxY < 0 || *data.MinY < 0 {
+		return c.Status(400).JSON(fiber.Map{`message`: `Invalid bounds.`})
 	}
 
 	rec := image.Rectangle{
@@ -120,124 +118,316 @@ func Crop(c *fiber.Ctx) error{
 			Y: *data.MaxY,
 		},
 	}
-	
-	file, err := c.FormFile(`image`);
+
+	file, err := c.FormFile(`image`)
 	if err != nil {
-		return fiber.NewError(500, `Could not open image.`);	 
+		return fiber.NewError(500, `Could not open image.`)
 	}
 
-	fileExt := strings.ToLower(filepath.Ext(file.Filename));
-	if fileExt != `png` && fileExt != `jpeg` && fileExt != `jpg`{
-		return fiber.NewError(400, `Expected png or jpg image.`); 
+	fileExt := strings.ToLower(filepath.Ext(file.Filename))
+	if fileExt != `png` && fileExt != `jpeg` && fileExt != `jpg` {
+		return fiber.NewError(400, `Expected png or jpg image.`)
 	}
-	
-	fileStream ,err := file.Open();
+
+	fileStream, err := file.Open()
 	if err != nil {
-		return fiber.NewError(500, `Could not open image.`); 
+		return fiber.NewError(500, `Could not open image.`)
 	}
-	defer fileStream.Close();
-	
-	var decodedImage image.Image;
-	decodedImage, err = imaging.Decode(fileStream);
-	
+	defer fileStream.Close()
+
+	var decodedImage image.Image
+	decodedImage, err = imaging.Decode(fileStream)
+
 	if err != nil {
 		slog.Error(`Could not decode image in crop handler. Error: ` + err.Error())
-		return fiber.NewError(500, `Something went wrong.`); 
+		return fiber.NewError(500, `Something went wrong.`)
 	}
 
-	if !rec.In(decodedImage.Bounds()){
-		return c.Status(400).JSON(fiber.Map{`message`:`Invalid bounds.`})
+	if !rec.In(decodedImage.Bounds()) {
+		return c.Status(400).JSON(fiber.Map{`message`: `Invalid bounds.`})
 	}
 
-	croppedImage := imaging.Crop(decodedImage, rec);	
-	
-	bodyWriter := c.Response().BodyWriter();
+	croppedImage := imaging.Crop(decodedImage, rec)
 
-	if fileExt == `png`{
-		err = imaging.Encode(bodyWriter, croppedImage, imaging.PNG);
-	}else{
-		err = imaging.Encode(bodyWriter, croppedImage, imaging.JPEG, imaging.JPEGQuality(85));
+	bodyWriter := c.Response().BodyWriter()
+
+	if fileExt == `png` {
+		err = imaging.Encode(bodyWriter, croppedImage, imaging.PNG)
+	} else {
+		err = imaging.Encode(bodyWriter, croppedImage, imaging.JPEG, imaging.JPEGQuality(85))
 	}
-	
+
 	if err != nil {
 		slog.Error(`Could not encode image in crop handler. Error: ` + err.Error())
-		return fiber.NewError(500, `Something went wrong.`); 
+		return fiber.NewError(500, `Something went wrong.`)
 	}
 
-	return nil;
+	return nil
 }
 
-type ResizeMetaData struct{
+type ResizeMetaData struct {
 	Height *int `json:"height"`
-	Width *int `json:"width"`
+	Width  *int `json:"width"`
 }
 
-func Resize(c *fiber.Ctx) error{
-	metadata := c.FormValue(`metadata`);
+func Resize(c *fiber.Ctx) error {
+	metadata := c.FormValue(`metadata`)
 
-	if metadata == ``{
-		return c.Status(400).JSON(fiber.Map{`message`:`Must set height or width to resize image.`})
+	if metadata == `` {
+		return c.Status(400).JSON(fiber.Map{`message`: `Must set height or width to resize image.`})
 	}
 
-	data := ResizeMetaData{};
-	err := json.Unmarshal([]byte(metadata), &data);
+	data := ResizeMetaData{}
+	err := json.Unmarshal([]byte(metadata), &data)
 	if err != nil {
 		slog.Error(`Could not unmarshal metadata in resize handler. Error: ` + err.Error())
-		return c.Status(500).JSON(fiber.Map{`message`:`Something went wrong.`})
+		return c.Status(500).JSON(fiber.Map{`message`: `Something went wrong.`})
 	}
 
-	if data.Width == nil && data.Height == nil{
-		return c.Status(400).JSON(fiber.Map{`message`:`Must set at least width or height to resize image.`})
+	if data.Width == nil && data.Height == nil {
+		return c.Status(400).JSON(fiber.Map{`message`: `Must set at least width or height to resize image.`})
 	}
 
-	fileHeader, err := c.FormFile(`image`);
+	fileHeader, err := c.FormFile(`image`)
 	if err != nil {
 		slog.Error(`Could not get form file in resize handler. Error: ` + err.Error())
-		return c.Status(500).JSON(fiber.Map{`message`:`Something went wrong.`})
+		return c.Status(500).JSON(fiber.Map{`message`: `Something went wrong.`})
 	}
 
-	extension := strings.ToLower(filepath.Ext(fileHeader.Filename));
+	extension := strings.ToLower(filepath.Ext(fileHeader.Filename))
 
-	if extension != `png` && extension != `jpeg` && extension != `jpg`{
-		return c.Status(400).JSON(fiber.Map{`message`:`Invalid extension.`});
+	if extension != `png` && extension != `jpeg` && extension != `jpg` {
+		return c.Status(400).JSON(fiber.Map{`message`: `Invalid extension.`})
 	}
 
-	fileStream, err := fileHeader.Open();
+	fileStream, err := fileHeader.Open()
 	if err != nil {
 		slog.Error(`Could not open image in resize handler. Error: ` + err.Error())
-		return c.Status(500).JSON(fiber.Map{`message`:`Something went wrong.`})
+		return c.Status(500).JSON(fiber.Map{`message`: `Something went wrong.`})
 	}
+	defer fileStream.Close()
 
-	decodedImage, err := imaging.Decode(fileStream);
+	decodedImage, err := imaging.Decode(fileStream)
 	if err != nil {
 		slog.Error(`Could not decode image in resize handler. Error: ` + err.Error())
-		return c.Status(500).JSON(fiber.Map{`message`:`Something went wrong.`})
+		return c.Status(500).JSON(fiber.Map{`message`: `Something went wrong.`})
 	}
 
-	var resizedImage *image.NRGBA;
-	if data.Width == nil && data.Height != nil && *data.Height > 0 && *data.Height < 2000{
-		resizedImage = imaging.Resize(decodedImage, 0, *data.Height, imaging.Lanczos);
-	}else if data.Height == nil && data.Width != nil && *data.Width > 0 && *data.Width < 2000{
-		resizedImage = imaging.Resize(decodedImage, *data.Width, 0, imaging.Lanczos);
-	}else if data.Height != nil && *data.Height > 0 && data.Width != nil && *data.Width > 0 && *data.Height < 2000 && *data.Width < 2000{
-		resizedImage = imaging.Resize(decodedImage, *data.Width, *data.Height, imaging.Lanczos);
-	}else{
-		return c.Status(400).JSON(fiber.Map{`message`:`Invalid parameters.`})
+	var resizedImage *image.NRGBA
+	if data.Width == nil && data.Height != nil && *data.Height > 0 && *data.Height < 2000 {
+		resizedImage = imaging.Resize(decodedImage, 0, *data.Height, imaging.Lanczos)
+	} else if data.Height == nil && data.Width != nil && *data.Width > 0 && *data.Width < 2000 {
+		resizedImage = imaging.Resize(decodedImage, *data.Width, 0, imaging.Lanczos)
+	} else if data.Height != nil && *data.Height > 0 && data.Width != nil && *data.Width > 0 && *data.Height < 2000 && *data.Width < 2000 {
+		resizedImage = imaging.Resize(decodedImage, *data.Width, *data.Height, imaging.Lanczos)
+	} else {
+		return c.Status(400).JSON(fiber.Map{`message`: `Invalid parameters.`})
 	}
 
 	writer := c.Request().BodyWriter()
 
-	if extension == `png`{
-		err = imaging.Encode(writer, resizedImage, imaging.PNG);
-	}else{
-		err = imaging.Encode(writer, resizedImage, imaging.JPEG, imaging.JPEGQuality(85));
+	if extension == `png` {
+		err = imaging.Encode(writer, resizedImage, imaging.PNG)
+	} else {
+		err = imaging.Encode(writer, resizedImage, imaging.JPEG, imaging.JPEGQuality(85))
 	}
 	if err != nil {
 		slog.Error(`Could node encode image in resize handler. Error: ` + err.Error())
+		return c.Status(500).JSON(fiber.Map{`message`: `Something went wrong.`})
+	}
+
+	return nil
+}
+
+type ChangeFormatMetadata struct {
+	FormatName *string `json:"formatName"`
+}
+
+func ChangeFormat(c *fiber.Ctx) error {
+	jsonPayload := c.FormValue(`metadata`)
+	var metadata = ChangeFormatMetadata{}
+	err := json.Unmarshal([]byte(jsonPayload), &metadata)
+	if err != nil {
+		slog.Error(`Could not unmarshal in change format handler. Error: ` + err.Error())
+		return c.Status(500).JSON(fiber.Map{`message`: `Something went wrong.`})
+	}
+
+	if metadata.FormatName == nil {
+		return c.Status(400).JSON(fiber.Map{`message`: `Must set format name.`})
+	}
+
+	formatName := strings.ToLower(*metadata.FormatName)
+
+	if formatName != `png` && formatName != `jpeg` && formatName != `jpg` {
+		return c.Status(400).JSON(fiber.Map{`message`: `Invalid format name.`})
+	}
+
+	fileheader, err := c.FormFile(`image`)
+	if err != nil {
+		slog.Error(`Could not get form file image in change format handler. Error: ` + err.Error())
+		return c.Status(500).JSON(fiber.Map{`message`: `Something went wrong.`})
+	}
+
+	extension := strings.ToLower(filepath.Ext(fileheader.Filename))
+	if extension != `png` && extension != `jpeg` || extension != `jpeg` {
+		return c.Status(400).JSON(fiber.Map{`message`: `Invalid file type.`})
+	}
+
+	if extension == formatName {
+		return c.Status(400).JSON(fiber.Map{`message`: `Invalid request.`})
+	}
+
+	file, err := fileheader.Open()
+	if err != nil {
+		slog.Error(`Could not open file in change format handler. Error: ` + err.Error())
+		return c.Status(500).JSON(fiber.Map{`message`: `Something went wrong.`})
+	}
+	defer file.Close()
+
+	image, err := imaging.Decode(file)
+	if err != nil {
+		slog.Error(`Could not decode image in change format handler. Error: ` + err.Error())
+		return c.Status(500).JSON(fiber.Map{`message`: `Something went wrong.`})
+	}
+
+	responseWriter := c.Response().BodyWriter()
+
+	if formatName == `png` {
+		err = imaging.Encode(responseWriter, image, imaging.PNG)
+	} else {
+		err = imaging.Encode(responseWriter, image, imaging.JPEG, imaging.JPEGQuality(85))
+	}
+
+	if err != nil {
+		slog.Error(`Could not encode image in change format handler. Error: ` + err.Error())
+		return c.Status(500).JSON(fiber.Map{`message`: `Something went wrong.`})
+	}
+
+	return nil
+}
+
+type FlipMetadata struct {
+	Direction *string `json:"direction"`
+}
+
+func Flip(c *fiber.Ctx) error {
+
+	metadata := c.FormValue(`metadata`)
+	if metadata == `` {
+		return c.Status(400).JSON(fiber.Map{`message`: `Must give direction to flip image.`})
+	}
+
+	data := FlipMetadata{}
+
+	err := json.Unmarshal([]byte(metadata), &data)
+	if err != nil {
+		slog.Error(`Could not unmarshal in flip handler. Error: ` + err.Error())
+		return c.Status(500).JSON(fiber.Map{`message`: `Something went wrong.`})
+	}
+
+	if data.Direction == nil {
+		return c.Status(400).JSON(fiber.Map{`message`: `Must set direction to flip image.`})
+	}
+
+	direction := strings.ToLower(*data.Direction)
+	if direction != `horizontal` && direction != `vertical` {
+		return c.Status(400).JSON(fiber.Map{`message`: `Direction of flip must be either horizontal or vertical.`})
+	}
+
+	fileheader, err := c.FormFile(`image`)
+	if err != nil {
+		slog.Error(`Could not get form file in flip handler. Error: ` + err.Error())
+		return c.Status(500).JSON(fiber.Map{`message`: `Something went wrong.`})
+	}
+
+	ext := strings.ToLower(filepath.Ext(fileheader.Filename))
+	if ext != `png` && ext != `jpeg` && ext != `jpg` {
+		return c.Status(400).JSON(fiber.Map{`message`: `Invalid file type.`})
+	}
+
+	file, err := fileheader.Open()
+	if err != nil {
+		slog.Error(`Could not open file in flip handler. Error: ` + err.Error())
+		return c.Status(500).JSON(fiber.Map{`message`: `Something went wrong.`})
+	}
+	defer file.Close()
+
+	image, err := imaging.Decode(file)
+	if err != nil {
+		slog.Error(`Could not decode image in flip handler. Error: ` + err.Error())
+		return c.Status(500).JSON(fiber.Map{`message`: `Something went wrong.`})
+	}
+
+	responseWriter := c.Response().BodyWriter()
+
+	if direction == `horizontal` {
+		flippedImage := imaging.FlipH(image)
+
+		if ext == `png` {
+			err = imaging.Encode(responseWriter, flippedImage, imaging.PNG)
+		} else {
+			err = imaging.Encode(responseWriter, flippedImage, imaging.JPEG, imaging.JPEGQuality(85))
+		}
+
+		if err != nil {
+			slog.Error(`Could not encode image in flip handler. Error: ` + err.Error())
+		}
+
+	} else if direction == `vertical` {
+		flippedImage := imaging.FlipV(image)
+
+		if ext == `png` {
+			err = imaging.Encode(responseWriter, flippedImage, imaging.PNG)
+		} else {
+			err = imaging.Encode(responseWriter, flippedImage, imaging.JPEG, imaging.JPEGQuality(85))
+		}
+
+		if err != nil {
+			slog.Error(`Could not encode image in flip handler. Error: ` + err.Error())
+		}
+	}
+
+	return nil
+}
+
+func GrayScale(c *fiber.Ctx) error{
+	fileheader, err := c.FormFile(`image`);
+	if err != nil {
+		slog.Error(`Could not get form file in gray scale handler. Error: ` + err.Error())
 		return c.Status(500).JSON(fiber.Map{`message`:`Something went wrong.`})
+	}
+
+	ext := strings.ToLower(filepath.Ext(fileheader.Filename));
+
+	if ext != `png` && ext != `jpeg` && ext != `jpg`{
+		return c.Status(400).JSON(fiber.Map{`message`:`Invalid file format.`})
+	}
+
+	file, err := fileheader.Open();
+	if err != nil {
+		slog.Error(`Could not open file in gray scale handler. Error: ` + err.Error())
+		return c.Status(500).JSON(fiber.Map{`message`:`Something went wrong.`})
+	}
+	defer file.Close();
+
+	image, err := imaging.Decode(file);
+	if err != nil {
+		slog.Error(`Could not decode image in gray scale handler. Error: ` + err.Error())
+		return c.Status(500).JSON(fiber.Map{`message`:`Something went wrong.`})
+	}
+
+	grayImage := imaging.Grayscale(image);
+	responseWriter := c.Response().BodyWriter();
+
+	if ext == `png`{
+		err = imaging.Encode(responseWriter, grayImage, imaging.PNG);
+	}else{
+		err = imaging.Encode(responseWriter, grayImage, imaging.JPEG, imaging.JPEGQuality(85));
+	}
+	if err != nil {
+		slog.Error(`Could not encode image gray scale handler. Error: ` + err.Error())
+		return c.Status(500).JSON(fiber.Map{`message`:`Something went wrong.`});
 	}
 
 	return nil;
 }
-
-
